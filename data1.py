9c4cfe6af14b0778bb52b0902e23eaa78e8d530c
key-lgmz <gomezkeyramychell@gmail.com>
2026-09-23 18:34:05 +0000

    data1.py

---
+++ data1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import streamlit as st
+#names_link = "https://raw.githubusercontent.com/key-lgmz/key-lgmz.github.io/refs/heads/main/pokemon_complete_stats.csv"
+#names_data = pd.read_csv(names_link)
+
+st.title("Streamlit and pandas")
+#st.dataframe(names_data)
